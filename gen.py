4affad33f3227e421e0249689e3eb6933d39f8da
Galerici <68757145+Galerici@users.noreply.github.com>
2020-08-23 01:32:50 +0300

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,56 @@
+import random
+import colorama 
+from colorama import Fore, Back, Style
+
+simvols="ABCDEFGHIKLMNOPQRSTVXYZ1234567890"#символы
+file=open("keys.txt","w")
+
+colorama.init()
+print(Fore.RED)
+
+print( """
+
+ ██████╗  █████╗ ██╗     ███████╗██████╗ ██╗ ██████╗██╗
+██╔════╝ ██╔══██╗██║     ██╔════╝██╔══██╗██║██╔════╝██║
+██║  ███╗███████║██║     █████╗  ██████╔╝██║██║     ██║
+██║   ██║██╔══██║██║     ██╔══╝  ██╔══██╗██║██║     ██║
+╚██████╔╝██║  ██║███████╗███████╗██║  ██║██║╚██████╗██║
+ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝
+                                                       
+ """ + Fore.RESET)
+print("Discord: discord.gg/HMMGKdC")
+
+
+
+while True:
+	try:
+		many=int(input("How many?: "))#кол-во ключей
+		break
+	except:
+		print("You are input not number!")
+
+while True:
+	try:
+		save_mode=int(input("How show keys?:\n1)in txt file(recommended)\n2)in console\n"))
+		break
+	except:
+		print("Please, input number!")
+
+for i in range(many):
+	key=""
+	for i1 in range(2):
+		for i2 in range(5):
+			key+=simvols[random.randint(1,len(simvols)-1)]
+		key+="-"
+	for i2 in range(5):
+		key+=simvols[random.randint(1,len(simvols)-1)]
+	
+	if save_mode==1:
+		file.write(key+"\n")
+	elif save_mode==2:	
+		print(key)
+
+file.close()
+print("\nKeys generating finished.You can close programm.")
+while True:
+	pass
